2b5e5d8942b329d9990238ae5036d6c17ed4a05e
Hans Guthrie <hansdg1@gmail.com>
2014-10-07 19:41:21 -0500

    First Commit

---
+++ remove_file_from_repo.sh
@@ -0,0 +1,16 @@
+#set path to working dir
+workingdir=$HOME/dotfiles.git
+filetoremove='megarc'
+repo=https://github.com/hansdg1/dotfiles.git
+git clone --mirror $repo 
+cd $workingdir
+
+wget 'https://repository.sonatype.org/service/local/repositories/central-proxy/content/com/madgag/bfg/1.11.8/bfg-1.11.8.jar'
+
+#https://rtyley.github.io/bfg-repo-cleaner/
+java -jar bfg-1.11.8.jar --delete-files $filetoremove .
+git reflog expire --expire=now --all
+git gc --prune=now --aggressive
+
+git push
+
